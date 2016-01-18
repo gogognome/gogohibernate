9e232650e8c28c054606e54ebba61379888e024e
Sander Kooijmans <sander@gogognome.nl>
2016-01-18 22:09:51 +0100

    All methods throw DataAccessException instead of unchecked exceptions.

--- src/main/java/nl/gogognome/gogohibernate/AbstractHibernateDAO.java
+++ src/main/java/nl/gogognome/gogohibernate/AbstractHibernateDAO.java
@@ -2,6 +2,7 @@ package nl.gogognome.gogohibernate;
 
 import nl.gogognome.dataaccess.DataAccessException;
 import org.hibernate.Criteria;
+import org.hibernate.LockMode;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.criterion.Order;
@@ -19,50 +20,74 @@ public abstract class AbstractHibernateDAO<E> {
         this.entityClass = entityClass;
     }
 
-    public E get(String id) {
-        return getSession().get(entityClass, id);
+    public E get(Serializable id) throws DataAccessException {
+        try {
+            return getSession().get(entityClass, id);
+        } catch (Exception e) {
+            throw new DataAccessException("Could not get instance of " + entityClass.getName() + " with id " + id, e);
+        }
     }
 
-    public Serializable save(E entity) {
-        return getSession().save(entity);
+    public Serializable save(E entity) throws DataAccessException {
+        try {
+            return getSession().save(entity);
+        } catch (Exception e) {
+            throw new DataAccessException("Could not save entity " + entityClass.getName(), e);
+        }
     }
 
     public boolean exists(Serializable id) throws DataAccessException {
-        Query query = getSession().createQuery("select count(*) from " + entityClass.getSimpleName() + " e where e.id = :id");
-        if (id instanceof  String) {
-            query.setString("id", (String) id);
-        } else if (id instanceof Integer) {
-            query.setLong("id", (Integer) id);
-        } else if (id instanceof Long) {
-            query.setLong("id", (Long) id);
-        } else {
-            throw new DataAccessException("Id of type " + id.getClass().getName() + " is not supported");
+        try {
+            Query query = getSession().createQuery("select count(*) from " + entityClass.getSimpleName() + " e where e.id = :id");
+            if (id instanceof String) {
+                query.setString("id", (String) id);
+            } else if (id instanceof Integer) {
+                query.setLong("id", (Integer) id);
+            } else if (id instanceof Long) {
+                query.setLong("id", (Long) id);
+            } else {
+                throw new DataAccessException("Id of type " + id.getClass().getName() + " is not supported");
+            }
+            return (Long) query.uniqueResult() > 0;
+        } catch (Exception e) {
+            throw new DataAccessException("Could not check existance of " + entityClass.getName() + " with id " + id, e);
         }
-        return (Long) query.uniqueResult() > 0;
     }
 
     protected Session getSession() {
         return session;
     }
 
-    public E update(E entity) throws DataAccessException {
-        if (!exists(getId(entity))) {
-            throw new DataAccessException(entityClass.getSimpleName() + ' ' + getId(entity) + " does not exist yet. Only existing entities can be updated.");
+    public void update(E entity) throws DataAccessException {
+        try {
+            if (!session.contains(entity)) {
+                getSession().lock(entity, LockMode.NONE);
+            }
+            session.save(entity);
+        } catch (Exception e) {
+            throw new DataAccessException("Could not update " + entityClass.getName(), e);
         }
-        return (E) getSession().merge(entity);
     }
 
     public void delete(E entity) throws DataAccessException {
-        if (!exists(getId(entity))) {
-            throw new DataAccessException(entityClass.getSimpleName() + ' ' + getId(entity) + " does not exist. Only existing entities can be deleted.");
+        try {
+            if (!session.contains(entity)) {
+                getSession().lock(entity, LockMode.NONE);
+            }
+            getSession().delete(entity);
+        } catch (Exception e) {
+            throw new DataAccessException("Could not delete " + entityClass.getName(), e);
         }
-        getSession().delete(entity);
     }
 
-    public List<E> findAll() {
-        Criteria c = getSession().createCriteria(entityClass);
-        c.addOrder(Order.asc(getIdColumn()));
-        return c.list();
+    public List<E> findAll() throws DataAccessException {
+        try {
+            Criteria c = getSession().createCriteria(entityClass);
+            c.addOrder(Order.asc(getIdColumn()));
+            return c.list();
+        } catch (Exception e) {
+            throw new DataAccessException("Could not find all instances of " + entityClass.getName(), e);
+        }
     }
 
     protected abstract Serializable getId(E entity);
